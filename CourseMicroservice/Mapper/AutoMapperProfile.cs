17aa95088b09c8ce4c26268ea8c9610e9cd3e12c
Michal Tulej <michaltulej1@gmail.com>
2025-06-12 09:47:11 +0200

    mapper added

---
+++ CourseMicroservice/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CourseMicroservice.Repositories.DatabaseModels;
+using SharedModels.Models.CourseModels.Request;
+using SharedModels.Models.CourseModels.Response;
+using SharedModels.Models.CourseModels.Response.GetAllCourses;
+using SharedModels.Models.CourseModels.Response.GetLessonById;
+
+namespace CourseMicroservice.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CreateCourseRequestModel, Course>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            CreateMap<CreateLessonRequestModel, Lesson>()
+                .ForMember(dest => dest.Id, opt => opt.Ignore());
+
+            CreateMap<Course, GetCourseByIdApiResponse>();
+            CreateMap<UpdateCourseRequestModel, Course>();
+            CreateMap<UpdateLessonRequestModel, Lesson>();
+
+
+            CreateMap<Lesson, LessonsApiResponse>();
+            CreateMap<Lesson, LessonApiResponse>();
+
+            CreateMap<Course, CourseList>();
+
+        }
+    }
+}
